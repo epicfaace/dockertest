e4ecf26407f3da1df62a45f553e6cab1fd5d62d4
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-05-14 09:40:33 -0400

    init docker test

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+docker pull alpine
+pip3 install -r requirements.txt
+python3 main.py
+"""
+
+import docker
+client = docker.from_env()
+client.networks.prune()
+client.networks.create("internal_network", internal=True)
+client.networks.create("external_network", internal=False)
+
+
+def run_ping(network):
+    print(f"starting docker container with network {network}...")
+    output = client.containers.run("alpine", "ping -c 1 google.com", network=network)
+    print(f"done. output is <{output.decode()}>")
+    print("")
+
+run_ping("external_network")
+run_ping("internal_network")
